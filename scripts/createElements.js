660dc26dba116d46adbe86f1e28497cf45c34ade
Breno Tardelli <brenot.dev@gmail.com>
2025-10-30 00:39:12 +0100

    responsive added

--- scripts/createElements.js
+++ scripts/createElements.js
@@ -6,20 +6,20 @@ export function createElements(displayReservations, reservations) {
     <div class="display">
             <div class="display-elements">
                <ul>
-                <li><p>Name Resevation: ${el.guestName}</p></li>
-                 <li><p>Guests: ${el.guestCount}</p></li>
-                 <li><p>Room Number: ${el.roomNumber}</p></li>
-                 <li><p>Room Type: ${el.roomType}</p></li>
-                 <li><p>Nights: ${el.nightsCount}</p></li>
-                 <li><p>Check-in: ${el.checkInDate}</p></li>
-                 <li><p>Check-out: ${el.checkOutDate}</p></li>
-                 <li><p>Total $: ${el.totalPrice}</p></li>
+                <li>Name: <span>${el.guestName}</span></li>
+                 <li>Guests: <span>${el.guestCount}</span></li>
+                 <li>Room #: <span>${el.roomNumber}</span></li>
+                 <li>Type: <span>${el.roomType}</span></li>
+                 <li>Nights: <span>${el.nightsCount}</span></li>
+                 <li>Check-in: <span>${el.checkInDate}</span></li>
+                 <li>Check-out: <span>${el.checkOutDate}</span></li>
+                 <li>Total $: <span>${el.totalPrice}</span></li>
                </ul>
              </div>
-             <div class="buttons">
+               <div class="buttons">
                 <a href="./pages/edit.html?id=${el.id}" class="btn-editar">edit</a>
                 <button class="btn-delete">delete</button>
-            </div>
+              </div>
     </div>
 `;
   });
