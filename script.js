8a7c03167dfc07cf227f2b326b222e16739c6640
Breno Tardelli <brenot.dev@gmail.com>
2025-11-03 23:19:22 +0100

    search list added

--- script.js
+++ script.js
@@ -30,11 +30,32 @@ window.addEventListener("load", async () => {
 
   searchBtn.addEventListener("click", () => {
     const searchValue = reservationNameId.value.toLowerCase();
-
+    displayReservations.innerHTML = "";
     const result = reservations.filter((data) => {
       return data.guestName.toLowerCase().includes(searchValue);
     });
 
-    console.log(result);
+    result.forEach((el, i) => {
+      displayReservations.innerHTML += `
+    <div class="display">
+            <div class="display-elements">
+               <ul>
+                <li>Name: <span>${el.guestName}</span></li>
+                 <li>Guests: <span>${el.guestCount}</span></li>
+                 <li>Room #: <span>${el.roomNumber}</span></li>
+                 <li>Type: <span>${el.roomType}</span></li>
+                 <li>Nights: <span>${el.nightsCount}</span></li>
+                 <li>Check-in: <span>${el.checkInDate}</span></li>
+                 <li>Check-out: <span>${el.checkOutDate}</span></li>
+                 <li>Total $: <span>${el.totalPrice}</span></li>
+               </ul>
+             </div>
+               <div class="buttons">
+                <a href="./pages/edit.html?id=${el.id}" class="btn-editar">edit</a>
+                <button class="btn-delete">delete</button>
+              </div>
+    </div>
+`;
+    });
   });
 });
